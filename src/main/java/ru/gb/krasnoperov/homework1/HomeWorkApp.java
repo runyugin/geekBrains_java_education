aa503c97d4cd1645345f21907ef1cc1d7c786da1
krasnoperov <runyugin@mail.ru>
2022-04-17 22:06:21 +0300

    HomeWork done

--- src/main/java/ru/gb/krasnoperov/homework1/HomeWorkApp.java
+++ src/main/java/ru/gb/krasnoperov/homework1/HomeWorkApp.java
@@ -1,7 +1,54 @@
 package ru.gb.krasnoperov.homework1;
 
 public class HomeWorkApp {
+
     public static void main(String[] args) {
 
+        printThreeWords();
+        checkSumSign();
+        printColor();
+        compareNumbers();
+    }
+
+    public static void printThreeWords() {
+        System.out.println("Orange");
+        System.out.println("Banana");
+        System.out.println("Apple");
+    }
+
+    public static void checkSumSign(){
+        int a = 8;
+        int b = 4;
+
+        if ((a+b)>=0) {
+            System.out.println("Сумма положительная");
+        } else {
+            System.out.println("Сумма отрицательная");
+        }
+    }
+
+    public static void printColor(){
+        int value = -101;
+
+        if (value<=0) {
+            System.out.println("Красный");
+        } else if (value>0 && value<=100){
+            System.out.println("Желтый");
+        } else if (value>100){
+            System.out.println("Зеленый");
+        }
+
+    }
+
+    public static void compareNumbers() {
+        int a =5;
+        int b= 0;
+
+
+        if (a>=b) {
+            System.out.println("a >= b");
+        } else {
+            System.out.println("a < b");
+        }
     }
 }
